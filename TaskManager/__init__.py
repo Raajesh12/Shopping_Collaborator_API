ccea4be6f89e48302a2ea7987f9dd171e288255a
Raajesh12 <raajstar3@gmail.com>
2017-08-08 16:25:06 -0500

    Created users endpoint POST

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -1,23 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask.views import MethodView
 import psycopg2
 import traceback
+from datetime import datetime
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(dbname="taskbase", user="www-data")
 
 @app.route("/", methods=['GET'])
 def home():
     return jsonify({'home': 'This is the homepage'}), 200
 
-@app.route("/add_task", methods=['POST'])
-def create_test():
-    conn = psycopg2.connect(dbname="ourbase", user="www-data")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return jsonify({'data':'data'}), 201
+class UserAPI(MethodView):
+    def post(self):
+        """
+        Example JSON for this request
+        {
+            'first_name':<first name>,
+            'last_name':<last_name>,
+            'email':<email>,
+            'password':<email>
+        }
+        """
+        json = request.get_json()
+        first_name = json['first_name']
+        last_name = json['last_name']
+        email = json['email']
+        password = json['password']
+        dt = datetime.now()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (first_name, last_name, email, password, created, last_modified) VALUES(%s, %s, %s, %s, %s, %s);", (first_name, last_name, email, password, dt, dt))
+        cur.commit()
+        cur.close()
 
 @app.route("/get_task", methods=['GET'])
 def get_test():
@@ -33,6 +48,9 @@ def get_test():
     cur.close()
     conn.close()
     return jsonify({'data': data}), 200
+
+user_view = UserAPI.as_view('user_api')
+app.add_url_rule('/users/', view_func=user_view, methods=['POST',])
 
 if __name__ == "__main__":
     app.run()
