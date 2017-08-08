d79bebb6a31a8025f47522307a39dbdb020601c9
Raajesh12 <raajstar3@gmail.com>
2017-08-08 16:51:17 -0500

    Imported flask

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, jsonify, request
 from flask.views import MethodView
 import psycopg2
@@ -33,7 +34,7 @@ class UserAPI(MethodView):
         cur.execute("INSERT INTO users (first_name, last_name, email, password, created, last_modified) VALUES (%s, %s, %s, %s, %s, %s);", (first_name, last_name, email, password, dt, dt))
         conn.commit()
         cur.close()
-        response = make_response()
+        response = flask.make_response()
         response.status = 201
         return response
 
