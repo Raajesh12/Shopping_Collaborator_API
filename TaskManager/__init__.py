363cacf346c83334b659388f027abb3d78fbc341
Raajesh12 <raajstar3@gmail.com>
2017-08-09 17:39:54 -0500

    get groups url changed uid to query parameter

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -109,16 +109,17 @@ class UserAPI(MethodView):
         return response
 
 class GroupAPI(MethodView):
-    def get(self, uid):
-        cur = conn.cursor()
-        cur.execute("SELECT (group_user_match.gid, groups.group_name) FROM group_user_match INNER JOIN groups ON group_user_match.gid = groups.gid WHERE group_user_match.uid=%s", (uid,))
-        data = {'groups' : []}
-        for row in cur:
-            row_tuple = make_tuple(row[0])
-            row_data = {'gid':row_tuple[0], 'group_name':row_tuple[1]}
-            data['groups'].append(row_data)
+    def get(self):
+        uid = request.args.get("uid")
+        # cur = conn.cursor()
+        # cur.execute("SELECT (group_user_match.gid, groups.group_name) FROM group_user_match INNER JOIN groups ON group_user_match.gid = groups.gid WHERE group_user_match.uid=%s", (uid,))
+        # data = {'groups' : []}
+        # for row in cur:
+            # row_tuple = make_tuple(row[0])
+            # row_data = {'gid':row_tuple[0], 'group_name':row_tuple[1]}
+            # data['groups'].append(row_data)
 
-        return jsonify(data), 200
+        return jsonify({'uid arg':uid}), 200
 
     def post(self):
         """
@@ -154,7 +155,7 @@ app.add_url_rule('/users/', view_func=user_view, methods=['POST',])
 app.add_url_rule('/users/<int:uid>', view_func=user_view, methods=['GET','PUT', 'DELETE'])
 
 group_view = GroupAPI.as_view('group_api')
-app.add_url_rule('/groups/<int:uid>', view_func=group_view, methods=['GET'])
+app.add_url_rule('/groups', view_func=group_view, methods=['GET'])
 app.add_url_rule('/groups/', view_func=group_view, methods=['POST',])
 app.add_url_rule('/groups/<int:gid>', view_func=group_view, methods=['PUT', 'DELETE'])
 
