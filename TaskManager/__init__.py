f882df0b8674c2589c4aad635109ce58a08c3652
Raajesh12 <raajstar3@gmail.com>
2017-08-09 20:23:00 -0500

    Removed GET

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -217,7 +217,7 @@ app.add_url_rule('/groups/', view_func=group_view, methods=['POST',])
 app.add_url_rule('/groups/<int:gid>', view_func=group_view, methods=['PUT', 'DELETE'])
 
 task_view = TaskAPI.as_view('task_api')
-app.add_url_rule('/tasks', view_func=task_view, methods=['POST', 'GET'])
+app.add_url_rule('/tasks', view_func=task_view, methods=['POST',])
 # app.add_url_rule('/tasks/<int:task_id>', view_func=task_view, methods=['DELETE',])
 
 if __name__ == "__main__":
