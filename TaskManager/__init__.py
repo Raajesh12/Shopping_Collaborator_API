abbe1f917e9b6607b9f956cf2fde1acc3d196686
Raajesh12 <raajstar3@gmail.com>
2017-08-22 17:41:18 -0500

    Added comma in dictionary

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -29,7 +29,7 @@ class UserAPI(MethodView):
         data = {
             'first_name': row[1],
             'last_name': row[2],
-            'email': row[3]
+            'email': row[3],
             'password': encrypt_functions.decrypt(row[4])
         }
         cur.close()
